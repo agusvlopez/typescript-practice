886d9c5b15a8c7487a58940d1ca551de6daca39d
agusvlopez <agusvlopez@gmail.com>
2024-03-08 00:02:51 -0300

    add: inferred types and scope

---
+++ src/02-typing.ts
@@ -0,0 +1,13 @@
+//Scope...
+(() => {
+  let myProductName = 'Product 1';
+  let myProductPrice = 123;
+
+  myProductName = 'change';
+  myProductName.toLowerCase();
+
+  myProductPrice.toFixed();
+
+  const myProductName2 = 'Product 2';
+  const myProductStock = 100;
+})();
